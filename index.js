2f287505e3afea09238277c8b7a34b66b7ecd121
Bryanp016 <97856535+Bryanp016@users.noreply.github.com>
2022-05-30 20:30:01 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+function validar(){
+    
+    
+    const codigo = document.getElementById("codigo").value; 
+    const titulo = document.getElementById("titulo").value; 
+    const autor = document.getElementById("autor").value; 
+    const editorial = document.getElementById("editorial").valy 
+    const fecha_publicacion = document.getElementById("fecha_publicacion").value; 
+    const fecha_ingreso = document.getElementById("fecha_ingreso").value;
+    
+
+    if(codigo == null || cedula.length == 0){
+    alert("*El campo Codigo) no puede estar vacío*"); 
+    console.log("Error campo de codigo vacío");
+    
+    if(codigo.length < 5){ alert("Debe ingresar 5 digitos");
+    return false;
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    }}}
